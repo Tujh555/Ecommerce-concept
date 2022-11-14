4431da952092a8dd7b8de64538d4b21982a45144
Tujh <egor.rusinov.com@gmail.com>
2022-11-15 01:50:22 +0500

    End all layouts

--- app/src/main/java/ru/effectivemobile/ecommerceconcept/MainActivity.kt
+++ app/src/main/java/ru/effectivemobile/ecommerceconcept/MainActivity.kt
@@ -1,18 +1,20 @@
 package ru.effectivemobile.ecommerceconcept
 
-import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import ru.effectivemobile.ecommerceconcept.databinding.CartFragmentBinding
-import ru.effectivemobile.ecommerceconcept.databinding.FilterOptionsFragmentBinding
-import ru.effectivemobile.ecommerceconcept.databinding.FragmentHomeBinding
+import androidx.recyclerview.widget.RecyclerView
+import ru.effectivemobile.ecommerceconcept.databinding.ProductDetailsFragmentBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {
-        CartFragmentBinding.inflate(layoutInflater)
+        ProductDetailsFragmentBinding.inflate(layoutInflater)
     }
     private val data = listOf(
         MobileData(1, "Iphone 12", "Lsdgorem ipsum dolem", "https://img.ibxk.com.br/2020/09/23/23104013057475.jpg?w=1120&h=420&mode=crop&scale=both"),
@@ -21,10 +23,61 @@ class MainActivity : AppCompatActivity() {
         MobileData(4, "Iphone 15", "Lorem sdipsum dolem")
     )
 
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+//        adapter.addColor(Color.MAGENTA)
+//        adapter.addColor(Color.RED)
+//        adapter.addColor(Color.parseColor("#772D03"))
+//        adapter.addColor(Color.parseColor("#010035"))
+//        adapter.addColor(Color.BLACK)
+//        adapter.addColor(Color.BLUE)
+//        adapter.addColor(Color.MAGENTA)
+
+        val adapter = ColorsAdapter().apply {
+            list.add(Color.MAGENTA)
+            list.add(Color.parseColor("#772D03"))
+            list.add(Color.parseColor("#010035"))
+            list.add(Color.BLACK)
+        }
+
+        binding.shopLayout.rvColors.adapter = adapter
+    }
+}
+
+class ColorsAdapter : RecyclerView.Adapter<ColorsAdapter.ColorsViewHolder>() {
+    var list = mutableListOf<Int>()
+    private var lastCheckedItem: CheckBox? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ColorsViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+
+        return ColorsViewHolder(
+            inflater.inflate(R.layout.color_select_list_item, parent, false) as CheckBox
+        )
+    }
+
+    override fun onBindViewHolder(holder: ColorsViewHolder, position: Int) {
+        holder.view.backgroundTintList = ColorStateList.valueOf(list[position])
+        holder.setCheckState()
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    inner class ColorsViewHolder(val view: CheckBox) : RecyclerView.ViewHolder(view) {
+        fun setCheckState() {
+            view.setOnClickListener {
+                view.isChecked = true
+                view.isClickable = false
+
+                lastCheckedItem?.isClickable = true
+                lastCheckedItem?.isChecked = false
+                lastCheckedItem = view
+            }
+        }
     }
 }
 
